2e52b507c57515c6120667f44f52cb8286255907
Nike-X <ngdfz1@gmail.com>
2016-07-08 19:17:48 +0300

    'AJAX2'

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+  require 'date'
+  
+  include CurrentCart
+  before_action :set_cart
   def index
     @products = Product.order(:title)
+    @date = DateTime.now
   end
 end
