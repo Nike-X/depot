0980676cd406aa4d33e1decf63e4d23d3e606556
Nike-X <ngdfz1@gmail.com>
2017-01-22 22:10:26 +0200

    Mailers added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,19 @@ Rails.application.configure do
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
+  
+  # Configure email sending settings
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    #domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "nickolay.kalashnikov023@gmail.com",
+    password: "Qazwsxedc12",
+    enable_starttls_auto: true
+  }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
